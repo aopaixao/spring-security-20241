b35a91a9b0ae9fb5f5b1dfad773692a7bd704314
aopaixao <aopaixao@outlook.com>
2024-05-20 19:07:46 -0300

    Implementação do UserDetails e do UserDetailsService

--- src/main/java/br/org/serratec/academia/services/UserDetailsServiceImpl.java
+++ src/main/java/br/org/serratec/academia/services/UserDetailsServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import br.org.serratec.academia.entities.User;
 import br.org.serratec.academia.entities.UserDetailsImpl;
 import br.org.serratec.academia.repositories.UserRepository;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired

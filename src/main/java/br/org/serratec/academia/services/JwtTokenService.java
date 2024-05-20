0f227b8c4d0f8b32e757a7f89792449a77f16953
aopaixao <aopaixao@outlook.com>
2024-05-20 15:23:48 -0300

    Inclusão do serviço responsável pelo token jwt

---
+++ src/main/java/br/org/serratec/academia/services/JwtTokenService.java
@@ -0,0 +1,50 @@
+package br.org.serratec.academia.services;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+import br.org.serratec.academia.entities.UserDetailsImpl;
+
+public class JwtTokenService {
+	private static final String SECRET_KEY = "asdfSFS34wfsdfsdfSDSD32dfsddDDerQSNCK34SOWEK5354fdgdf4";  
+
+    private static final String ISSUER = "serratec.org.br"; 
+
+    public String generateToken(UserDetailsImpl user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+            return JWT.create()
+                    .withIssuer(ISSUER) 
+                    .withIssuedAt(new Date()) 
+                    .withExpiresAt(expirationDate()) 
+                    .withSubject(user.getUsername()) 
+                    .sign(algorithm); 
+        } catch (JWTCreationException exception){
+            throw new JWTCreationException("Erro ao gerar token.", exception);
+        }
+    }
+
+    public String getSubjectFromToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+            return JWT.require(algorithm)
+                    .withIssuer(ISSUER) 
+                    .build()
+                    .verify(token) 
+                    .getSubject(); 
+        } catch (JWTVerificationException exception){
+            throw new JWTVerificationException("Token inválido ou expirado.");
+        }
+    }
+
+    private Instant expirationDate() {
+        return ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).plusHours(2).toInstant();
+    }
+}

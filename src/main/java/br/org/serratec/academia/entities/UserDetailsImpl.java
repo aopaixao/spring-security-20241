32bfcdab9cb367b44980ceeeb1f3d859b7cfcba8
aopaixao <aopaixao@outlook.com>
2024-05-21 09:24:47 -0300

    Criação da classe de config. de segurança; do filter; dos controllers e dos records

--- src/main/java/br/org/serratec/academia/entities/UserDetailsImpl.java
+++ src/main/java/br/org/serratec/academia/entities/UserDetailsImpl.java
@@ -7,27 +7,10 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 public class UserDetailsImpl implements UserDetails {
 	private static final long serialVersionUID = 1L;
+
 	private User user;
-	private Integer id;
-	private String username;
-	private String email;
-	@JsonIgnore
-	private String password;
-
-	private Collection<? extends GrantedAuthority> authorities;
-
-	public UserDetailsImpl(Integer id, String username, String email, String password,
-			Collection<? extends GrantedAuthority> authorities) {
-		this.id = id;
-		this.username = username;
-		this.email = email;
-		this.password = password;
-		this.authorities = authorities;
-	}	
 
 	public UserDetailsImpl(User user) {
 		this.user = user;
@@ -42,20 +25,11 @@ public class UserDetailsImpl implements UserDetails {
 		return user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName().name()))
 				.collect(Collectors.toList());
 	}
-	
-	
 
 	public User getUser() {
 		return user;
 	}
 
-	public Integer getId() {
-		return id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
 
 	@Override
 	public String getPassword() {
